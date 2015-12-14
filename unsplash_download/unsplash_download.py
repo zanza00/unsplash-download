f7e71debc77c8d26d68e156092328c29eb50260c
Zanza <zanza00@gmail.com>
2015-12-14 15:59:57 +0100

    modify cicle for support of arbitrary number of downloaded images

--- unsplash_download/unsplash_download.py
+++ unsplash_download/unsplash_download.py
@@ -19,6 +19,8 @@ import urllib.request
 from docopt import docopt
 from bs4 import BeautifulSoup, SoupStrainer
 
+from math import ceil
+
 DEBUG = False
 ud_version = '1.0.2'
 
@@ -26,18 +28,18 @@ ud_version = '1.0.2'
 # download_path = arguments['<folder>']
 download_path = 'download'
 base_url = 'https://unsplash.com'
-photos_to_download = 1
+photos_to_download = 35 # TODO implement arbitrary number
 link_search = re.compile("/photos/[a-zA-Z0-9-]+/download")
 
 if not os.path.exists(download_path):
     os.makedirs(download_path)
 
-for page in range(1, 2):
+for page in range(1, ceil(photos_to_download/20)):
     url = base_url + "/?page=" + str(page)
     print("Parsing page %s" % url)
     try:
         soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
-        for tag in soup.find_all(href=link_search):
+        for current_img_number, tag in enumerate(soup.find_all(href=link_search)):
             image_id = str(tag['href']).split('/')[2]
             download_url = base_url + str(tag['href'])
 
